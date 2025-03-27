88580545709f8050648f0a7be41cbe1fc43d759b
crtusr <lucasgasperi03@gmail.com>
2025-03-27 16:01:14 -0300

    Update and rename istr.h to inputfields.h

---
+++ C/inputfields.h
@@ -0,0 +1,42 @@
+#ifndef INPUTFIELDS_H
+#define INPUTFIELDS_H
+
+#include <stdbool.h>
+
+#define MAX_FIELDS 3 
+
+typedef enum {
+    STRING,
+    INTEGER,
+    FLOAT
+} InputFieldType;
+
+typedef struct {
+    char* prompt;
+    char input_buffer[100]; // Buffer para la entrada del usuario
+    int max_length;
+    bool password_mode;
+    int start_x;
+    int start_y;
+    int count;
+    int cursor_pos;
+    int type;
+} InputField;
+
+void init_input_field(InputField* field, const char* prompt, int max_length, bool password_mode, int start_x, int start_y, int type);
+
+void draw_input_field(const InputField* field);
+
+static void insert_char(InputField* field, char ch);
+
+void handle_input_char(InputField* field, int ch);
+
+void handle_backspace(InputField* field);
+
+void handle_cursor_left(InputField* field);
+
+void handle_cursor_right(InputField* field);
+
+void input_fields_loop(InputField fields[], int num_fields); // Function to handle input loop for fields
+
+#endif
--- C/istr.h
+++
@@ -1,8 +0,0 @@
-#ifndef ISTR_H
-#define ISTR_H
-
-#include <stdbool.h>
-
-char* get_limited_input(int max_length, const char* prompt, bool password_mode, int start_x, int start_y);
-
-#endif
