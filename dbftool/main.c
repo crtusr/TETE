32337cd72a76e97560e919f15194707deebbc022
crtusr <lucasgasperi03@gmail.com>
2025-04-11 14:24:58 -0300

    Update main.c

--- dbftool/main.c
+++ dbftool/main.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 #include "dbftool.h"
 #include <string.h>
+#include <inttypes.h>
+#include <errno.h>
 
-#define FIELD_SIZE 32L
-#define FIELD_NAME 10L
-#define FIELD_TYPE 1L
-#define RESERVED_BYTES 4L
-#define FIELD_LENGTH 1L
-#define DECIMAL_COUNT 1L
+
+/*
+I will test the functions here
+
+Probare las funciones aquí
+*/
 
 int main() {
 
+header header[5]; //5 is an arbitrary value 
+
+descriptor descriptor[MAX_DBF_FIELDS];
+
 FILE *stockptr;
-unsigned char buffer[255];
+unsigned char buffer[MAX_FIELD_LENGTH];
 int cant_campos = 0;
 
-memset(buffer, 0, 255);
+memset(buffer, 0, 254);
 
-
-
+printf("  %d  ", sizeof(header));
 
 stockptr = fopen("STOCK1.dbf", "rb");
 
@@ -30,22 +35,32 @@ if (stockptr == NULL) {
 	}
 
 
+	store_header_data(header, stockptr, 0);
+
+	store_descriptor_data(descriptor, stockptr);
+
+	get_data(buffer , get_index("NOMBRE", "ABUTI", stockptr, header, descriptor), "PRECIOB", stockptr, header, 
+
+descriptor);	
+
+printf(" esta en indice %d\ndato obtenido: %s", get_index("NOMBRE", "ABUTI", stockptr, header, descriptor), buffer);
+
+//printf("%x %x %x %x %d %d %d", header[0].dbfversion, header[0].date[0], header[0].date[1], header[0].date[2], header
+
+[0].nofrecords, header[0].header_bytes, header[0].record_bytes);
 
 
 printf("STOCK1.dbf abierto correctamente\n\n");
 
-fseek(stockptr, FIELD_SIZE, SEEK_SET);
+ int i = 0;
 
-int read = fread(buffer, 1, FIELD_NAME, stockptr);
+while (descriptor[i].fieldname[0] != 0X0d) {
+	
 
-while (buffer[0] != 0x0d) {
+	printf("%s\n", descriptor[i].fieldname);
 
-	printf("%s\n", buffer);
-
-	fseek(stockptr, FIELD_SIZE - FIELD_NAME, SEEK_CUR);
-
-	read = fread(buffer, 1, FIELD_NAME, stockptr);
-
+	
+	i++;
 	cant_campos++;
 	
 }
