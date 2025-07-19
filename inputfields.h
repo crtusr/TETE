5459282ec4f18b39c0c705475814ff584a43d946
crtusr <lucasgasperi03@gmail.com>
2025-07-19 12:36:45 -0300

    Update inputfields.h

--- inputfields.h
+++ inputfields.h
@@ -8,7 +8,8 @@
 typedef enum {
     STRING,
     INTEGER,
-    FLOAT
+    FLOAT,
+    DATE
 } InputFieldType;
 
 typedef struct {
@@ -24,17 +25,12 @@ typedef struct {
 } InputField;
 
 void init_input_field(InputField* field, const char* prompt, int max_length, bool password_mode, int start_x, int start_y, int type);
-
 void draw_input_field(const InputField* field);
-
+void draw_date_field(const InputField* field);
 void handle_input_char(InputField* field, int ch);
-
 void handle_backspace(InputField* field);
-
 void handle_cursor_left(InputField* field);
-
 void handle_cursor_right(InputField* field);
-
 void input_fields_loop(InputField fields[], int num_fields, void (*background)(void)); // Function to handle input loop for fields
 
 #endif
