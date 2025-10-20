1d72083549b1a155796208b4afa4e25c43b33857
crtusr <lucasgasperi03@gmail.com>
2025-10-20 13:28:29 -0300

    Update menu.h

--- C/menu.h
+++ C/menu.h
@@ -20,5 +20,6 @@ void recuadro (int start_x, int start_y, int width, int height);
 
 int dibujar_menu(MenuItem *menu, int selected_row, int selected_col, int level);
 int yesNoMenu(char *yes, int yesX, int yesY, char* no, int noX, int noY);
+int printingMenu();
 
 #endif
